79432c117c192bf93b1378b34c5737acc67663e5
Elian Doran <contact@eliandoran.me>
2026-01-17 10:01:12 +0200

    chore(standalone-desktop): trigger Neutralino build

--- apps/standalone-desktop/scripts/build.ts
+++ apps/standalone-desktop/scripts/build.ts
@@ -1,9 +1,12 @@
+import { execSync } from "child_process";
 import BuildHelper from "../../../scripts/build-utils";
 
 const build = new BuildHelper("apps/standalone-desktop");
 
 async function main() {
     build.triggerBuildAndCopyTo("apps/client-standalone", "../resources");
+    execSync("pnpm neu update", { cwd: build.projectDir, stdio: "inherit" });
+    execSync("pnpm neu build", { cwd: build.projectDir, stdio: "inherit" });
 }
 
 main();
