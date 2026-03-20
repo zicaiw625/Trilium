fc247fe790aa38426666bac0b236f962495f8e2f
Elian Doran <contact@eliandoran.me>
2026-03-20 17:34:29 +0200

    feat(lexical): add very basic integration

--- apps/client/src/widgets/type_widgets/text/lexical/index.tsx
+++ apps/client/src/widgets/type_widgets/text/lexical/index.tsx
@@ -1,5 +1,44 @@
+import {AutoFocusPlugin} from '@lexical/react/LexicalAutoFocusPlugin';
+import {LexicalComposer} from '@lexical/react/LexicalComposer';
+import {ContentEditable} from '@lexical/react/LexicalContentEditable';
+import {LexicalErrorBoundary} from '@lexical/react/LexicalErrorBoundary';
+import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
+import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
+
 import { TypeWidgetProps } from "../../type_widget";
 
+const theme = {
+    // Theme styling goes here
+    //...
+};
+
+// Catch any errors that occur during Lexical updates and log them
+// or throw them as needed. If you don't throw them, Lexical will
+// try to recover gracefully without losing user data.
+function onError(error) {
+    console.error(error);
+}
+
 export default function LexicalText({}: TypeWidgetProps) {
-    return <p>Hi</p>;
+    const initialConfig = {
+        namespace: 'MyEditor',
+        theme,
+        onError,
+    };
+
+    return (
+        <LexicalComposer initialConfig={initialConfig}>
+            <RichTextPlugin
+                contentEditable={
+                    <ContentEditable
+                        aria-placeholder={'Enter some text...'}
+                        placeholder={<div>Enter some text...</div>}
+                    />
+                }
+                ErrorBoundary={LexicalErrorBoundary}
+            />
+            <HistoryPlugin />
+            <AutoFocusPlugin />
+        </LexicalComposer>
+    );
 }
